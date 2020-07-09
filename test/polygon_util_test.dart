4185f9da627a812be95e96d39139ecff63d671a1
Kirill Bychkov <bychkov.kirill.0@gmail.com>
2020-07-09 14:07:12 +0300

    add tests for PolygonUtil.isLocationOnEdge

--- test/polygon_util_test.dart
+++ test/polygon_util_test.dart
@@ -43,6 +43,43 @@ void assertLineLength(List<LatLng> line, List<LatLng> simplifiedLine) {
   }
 }
 
+List<LatLng> makeList([List<num> coords]) {
+  final size = coords.length ~/ 2;
+
+  final list = <LatLng>[];
+  for (var i = 0; i < size; ++i) {
+    list.add(LatLng(coords[i + i].toDouble(), coords[i + i + 1].toDouble()));
+  }
+  return list;
+}
+
+void onEdgeCaseWithGeodesic(
+    bool geodesic, List<LatLng> poly, List<LatLng> yes, List<LatLng> no) {
+  for (final point in yes) {
+    expect(PolygonUtil.isLocationOnEdge(point, poly, geodesic), equals(true));
+    expect(PolygonUtil.isLocationOnPath(point, poly, geodesic), equals(true));
+  }
+  for (final point in no) {
+    expect(PolygonUtil.isLocationOnEdge(point, poly, geodesic), equals(false));
+    expect(PolygonUtil.isLocationOnPath(point, poly, geodesic), equals(false));
+  }
+}
+
+void onEdgeCase(List<LatLng> poly, List<LatLng> yes, List<LatLng> no) {
+  onEdgeCaseWithGeodesic(true, poly, yes, no);
+  onEdgeCaseWithGeodesic(false, poly, yes, no);
+}
+
+void locationIndexCaseWithGeodesic(
+    bool geodesic, List<LatLng> poly, LatLng point, int idx) {
+  expect(PolygonUtil.locationIndexOnPath(point, poly, geodesic), idx);
+}
+
+void locationIndexCase(List<LatLng> poly, LatLng point, int idx) {
+  locationIndexCaseWithGeodesic(true, poly, point, idx);
+  locationIndexCaseWithGeodesic(false, poly, point, idx);
+}
+
 void main() {
   // The vertices of an octahedron, for testing
   final polygon = [
@@ -166,5 +203,121 @@ void main() {
     assertEndPoints(oval, simplifiedClosedOval);
     assertSimplifiedPointsFromLine(oval, simplifiedClosedOval);
     assertLineLength(oval, simplifiedClosedOval);
+  });
+
+  test('on edge', () {
+    // Empty
+    onEdgeCase([], [], [LatLng(0, 0)]);
+
+    const small = 5e-7; // About 5cm on equator, half the default tolerance.
+    const big = 2e-6; // About 10cm on equator, double the default tolerance.
+
+    // Endpoints
+    onEdgeCase(makeList([1, 2]), makeList([1, 2]), makeList([3, 5]));
+    onEdgeCase(
+        makeList([1, 2, 3, 5]), makeList([1, 2, 3, 5]), makeList([0, 0]));
+
+    // On equator.
+    onEdgeCase(
+        makeList([0, 90, 0, 180]),
+        makeList(
+            [0, 90 - small, 0, 90 + small, 0 - small, 90, 0, 135, small, 135]),
+        makeList([0, 90 - big, 0, 0, 0, -90, big, 135]));
+
+    // Ends on same latitude.
+    onEdgeCase(
+        makeList([-45, -180, -45, -small]),
+        makeList([
+          -45,
+          180 + small,
+          -45,
+          180 - small,
+          -45 - small,
+          180 - small,
+          -45,
+          0
+        ]),
+        makeList([-45, big, -45, 180 - big, -45 + big, -90, -45, 90]));
+
+    // Meridian.
+    onEdgeCase(
+        makeList([-10, 30, 45, 30]),
+        makeList([10, 30 - small, 20, 30 + small, -10 - small, 30 + small]),
+        makeList([-10 - big, 30, 10, -150, 0, 30 - big]));
+
+    // Slanted close to meridian, close to North pole.
+    onEdgeCase(
+        makeList([0, 0, 90 - small, 0 + big]),
+        makeList([1, 0 + small, 2, 0 - small, 90 - small, -90, 90 - small, 10]),
+        makeList([-big, 0, 90 - big, 180, 10, big]));
+
+    // Arc > 120 deg.
+    onEdgeCase(
+        makeList([0, 0, 0, 179.999]),
+        makeList([0, 90, 0, small, 0, 179, small, 90]),
+        makeList([0, -90, small, -100, 0, 180, 0, -big, 90, 0, -90, 180]));
+
+    onEdgeCase(
+        makeList([10, 5, 30, 15]),
+        makeList([
+          10 + 2 * big,
+          5 + big,
+          10 + big,
+          5 + big / 2,
+          30 - 2 * big,
+          15 - big
+        ]),
+        makeList([
+          20,
+          10,
+          10 - big,
+          5 - big / 2,
+          30 + 2 * big,
+          15 + big,
+          10 + 2 * big,
+          5,
+          10,
+          5 + big
+        ]));
+
+    onEdgeCase(
+        makeList([90 - small, 0, 0, 180 - small / 2]),
+        makeList(
+            [big, -180 + small / 2, big, 180 - small / 4, big, 180 - small]),
+        makeList([-big, -180 + small / 2, -big, 180, -big, 180 - small]));
+
+    // Reaching close to North pole.
+    onEdgeCaseWithGeodesic(
+        true,
+        makeList([80, 0, 80, 180 - small]),
+        makeList([90 - small, -90, 90, -135, 80 - small, 0, 80 + small, 0]),
+        makeList([80, 90, 79, big]));
+
+    onEdgeCaseWithGeodesic(
+        false,
+        makeList([80, 0, 80, 180 - small]),
+        makeList([80 - small, 0, 80 + small, 0, 80, 90]),
+        makeList([79, big, 90 - small, -90, 90, -135]));
+  });
+
+  test('location index', () {
+    locationIndexCase(makeList([]), LatLng(0, 0), -1);
+
+    // One point.
+    locationIndexCase(makeList([1, 2]), LatLng(1, 2), 0);
+    locationIndexCase(makeList([1, 2]), LatLng(3, 5), -1);
+
+    // Two points.
+    locationIndexCase(makeList([1, 2, 3, 5]), LatLng(1, 2), 0);
+    locationIndexCase(makeList([1, 2, 3, 5]), LatLng(3, 5), 0);
+    locationIndexCase(makeList([1, 2, 3, 5]), LatLng(4, 6), -1);
+
+    // Three points.
+    locationIndexCase(makeList([0, 80, 0, 90, 0, 100]), LatLng(0, 80), 0);
+    locationIndexCase(makeList([0, 80, 0, 90, 0, 100]), LatLng(0, 85), 0);
+    locationIndexCase(makeList([0, 80, 0, 90, 0, 100]), LatLng(0, 90), 0);
+    locationIndexCase(makeList([0, 80, 0, 90, 0, 100]), LatLng(0, 95), 1);
+    locationIndexCase(makeList([0, 80, 0, 90, 0, 100]), LatLng(0, 100), 1);
+    locationIndexCase(makeList([0, 80, 0, 90, 0, 100]), LatLng(0, 110), -1);
   });
 }
