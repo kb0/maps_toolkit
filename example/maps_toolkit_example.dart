ce4b0e7ead97252af7b3f7eaa6cc31247632dc1c
James Leahy <defuncart@gmail.com>
2023-12-07 12:20:33 +0100

    feature: LatLng has a const constructor

--- example/maps_toolkit_example.dart
+++ example/maps_toolkit_example.dart
@@ -1,8 +1,8 @@
 import 'package:maps_toolkit/maps_toolkit.dart';
 
 void main() {
-  final cityLondon = LatLng(51.5073509, -0.1277583);
-  final cityParis = LatLng(48.856614, 2.3522219);
+  const cityLondon = LatLng(51.5073509, -0.1277583);
+  const cityParis = LatLng(48.856614, 2.3522219);
 
   final distance =
       SphericalUtil.computeDistanceBetween(cityLondon, cityParis) / 1000.0;
